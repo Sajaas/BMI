ba6a1f379c7dff68dd06c6d0e844cce40eae23dd
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-08-29 14:52:00 +0200

    added UI tests for imperial. Added css

--- spec/bmi_ui_spec.js
+++ spec/bmi_ui_spec.js
@@ -5,18 +5,26 @@ describe('BMI_UI - index.html', function() {
         $.holdReady(false);
         $('#weight').val('90');
         $('#height').val('186');
-        $('#weight_pounds')
-        $('#height_feets')
-        $('#height_inches')
+        $('#weight_pounds').val('198.42');
+        $('#height_feets').val('6');
+        $('#height_inches').val('1.23');
         $('#calculate').trigger('click');
         $('#calculate_2').trigger('click');
     });
 
-    it("displays BMI Value", function() {
+    it("displays BMI value (metric)", function() {
         expect($('#display_value').text()).toBe('Your BMI is 26.01');
     });
-    it("displays BMI message", function() {
+    it("displays BMI message (metric)", function() {
         expect($('#display_message').text()).toBe('and you are Overweight');
+    });
+
+    it("displays BMI value (imperial)", function() {
+        expect($('#display_value_2').text()).toBe('Your BMI is 26.01');
+    });
+
+    it("displays BMI message (imperial)", function() {
+        expect($('#display_message_2').text()).toBe('and you are Overweight');
     });
 
 });
